99f675302f2af9212e8618d7aae422bfdb8904f5
Wex <v.pintaric94@gmail.com>
2019-12-04 21:58:58 +0100

    Fourth day

---
+++ d04/s01.cpp
@@ -0,0 +1,81 @@
+//
+// Created by wex on 12/2/19.
+//
+
+#include <iostream>
+#include <vector>
+
+void increment( std::vector< int > &digits ) {
+    for ( auto iter = digits.rbegin(); iter != digits.rend(); ++iter ) {
+        auto &digit = *iter;
+        ++digit;
+        if ( digit == 10 ) {
+            digit = 0;
+        } else {
+            break;
+        }
+    }
+}
+
+void setToNextValidNumber( std::vector< int > &digits ) {
+    increment( digits );
+
+    int previous = 0;
+    bool hasRepeating = false;
+    int i;
+    for ( i = 0; i < digits.size(); ++i ) {
+        if ( digits[ i ] < previous ) {
+            break;
+        }
+        if ( digits[ i ] == previous ) {
+            hasRepeating = true;
+        }
+        previous = digits[ i ];
+    }
+
+    for ( ; i < digits.size(); ++i ) {
+        digits[ i ] = previous;
+        hasRepeating = true;
+    }
+
+    if ( !hasRepeating ) {
+        digits[ digits.size() - 2 ] = digits[ digits.size() - 1 ];
+    }
+}
+
+int digitsToInt( const std::vector< int > &digits ) {
+    int base = 1;
+    int num = 0;
+    for ( auto iter = digits.rbegin(); iter != digits.rend(); ++iter ) {
+        num += *iter * base;
+        base *= 10;
+    }
+
+    return num;
+}
+
+int main() {
+    constexpr int N_DIGITS = 6;
+
+    int limit;
+
+    std::vector< int > digits;
+    digits.reserve( N_DIGITS );
+    for ( int i = 0; i < N_DIGITS; ++i ) {
+        digits.push_back( std::cin.get() - '0' );
+    }
+    std::cin.ignore( 1 );
+    std::cin >> limit;
+
+    setToNextValidNumber( digits );
+    int count = 0;
+    while ( digitsToInt( digits ) < limit ) {
+        ++count;
+        std::cout << digitsToInt( digits ) << std::endl;
+        setToNextValidNumber( digits );
+    }
+
+    std::cout << count << std::endl;
+
+    return 0;
+}
